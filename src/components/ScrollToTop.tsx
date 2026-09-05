6bdbb346fce6be5252de746c3a1ab6263248ada4
aman-pallet <aman.r@palletnow.co>
2026-09-05 22:34:42 +0530

    cross payment related fixes

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -19,9 +19,6 @@ export default function ScrollToTop(): null {
   const navigationType = useNavigationType();
 
   useEffect(() => {
-    if (document.activeElement instanceof HTMLElement) {
-      document.activeElement.blur();
-    }
     if (navigationType === 'POP') return; // browser restores back/forward scroll
     if (hash) {
       const el = document.getElementById(hash.slice(1));
